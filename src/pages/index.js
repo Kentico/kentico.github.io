efe9997f248b2946ae91886418c2d32ae549ae25
Simply007 <chrastina.ondra@gmail.com>
2018-09-25 16:44:22 +0200

    travis test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ const IndexPage = () => (
   <Layout>
     <h1>Kentico Developer Community</h1>
     <p>We help developers help each other.</p>
-    <p>The site is under construction :-) This state is temporary, we promise! - Developer Community Team</p>
+    <p>The site is under construction :-)</p>
+    <p> This state is temporary, we promise! - Developer Community Team</p>
     <p><strong>#ICODETHEREFOREIAM</strong></p>
   </Layout>
 )
