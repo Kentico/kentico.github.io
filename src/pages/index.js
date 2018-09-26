de08dc51d3f6950242d71d73a2ddb4e7432a55e1
Simply007 <chrastina.ondra@gmail.com>
2018-09-26 10:19:57 +0200

    add gif under construction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import UnderConstruction from '../images/under-construction.gif'
 
 import Layout from '../components/layout'
 
@@ -7,6 +8,7 @@ const IndexPage = () => (
     <h1>Kentico Developer Community</h1>
     <p>We help developers help each other.</p>
     <p>The site is under construction :-)</p>
+    <img src={UnderConstruction} alf="Under construction"/>
     <p> This state is temporary, we promise! your Developer Community Team</p>
     <p><strong>#ICODETHEREFOREIAM</strong></p>
   </Layout>
