e543fed98849de4cdeaa9063aa18e4ce9afc95b7
Simply007 <chrastina.ondra@gmail.com>
2018-11-23 14:04:40 +0100

    Updates

--- sw.js
+++ sw.js
@@ -29,18 +29,18 @@ self.__precacheManifest = [
     "url": "webpack-runtime-79136f9858cb0af8f72f.js"
   },
   {
-    "url": "app-30757eb8c05cdfef822c.js"
+    "url": "app-50dcfb86dd135a395019.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-daa6a38c24810a50bf8e.js"
   },
   {
     "url": "index.html",
-    "revision": "0982096ecea9b08f4a4b2a9936d08519"
+    "revision": "5742ff80fd650a19b3c761cbd78f8ef7"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "94d0b3d68a7ac60a250c05e3b3b12f6f"
+    "revision": "24e58026f0dd03a3567a7c094fe2d1e8"
   },
   {
     "url": "component---src-pages-index-js.63abbf0d467343a779ce.css"
@@ -79,7 +79,11 @@ workbox.routing.registerNavigationRoute("/offline-plugin-app-shell-fallback/inde
   blacklist: [/\?(.+&)?no-cache=1$/],
 });
 
-workbox.routing.registerRoute(/^https:\/\/communitypagegithubdata.table.core.windows.net\/githubStatus\//, workbox.strategies.staleWhileRevalidate({ "cacheName":"gihub-data", plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]}), new workbox.expiration.Plugin({"maxEntries":10,"maxAgeSeconds":3600,"purgeOnQuotaError":false})] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/communitypagegithubdata.table.core.windows.net\/githubStatus\//, workbox.strategies.staleWhileRevalidate({ "cacheName":"gihub-data", plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]}), new workbox.expiration.Plugin({"maxAgeSeconds":3600,"maxEntries":30,"purgeOnQuotaError":false})] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/assets-us-01.kc-usercontent.com\//, workbox.strategies.staleWhileRevalidate({ "cacheName":"kc-assets-data", plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, workbox.strategies.staleWhileRevalidate({ "cacheName":"google-fonts-stylesheets", plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/fonts\.gstatic\.com/, workbox.strategies.cacheFirst({ "cacheName":"google-fonts-webfonts", plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]}), new workbox.expiration.Plugin({"maxAgeSeconds":3600,"maxEntries":30,"purgeOnQuotaError":false})] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/kentico.github.io/, workbox.strategies.staleWhileRevalidate({ "cacheName":"local-assets", plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
 "use strict";
 
 /* global workbox */
