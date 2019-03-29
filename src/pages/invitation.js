acad35a460b53f4d184b1446edf2b23ce19ffd7a
Simply007 <chrastina.ondra@gmail.com>
2019-03-29 14:17:44 -0400

    Initial design

---
+++ src/pages/invitation.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby'
+
+import Invitation from '../components/invitation';
+
+
+const InvitationPage = () => (
+  <StaticQuery
+    query={graphql`
+{
+  kenticoCloudItemInvitationPage(system: {codename: {eq: "kentico_invitation_page"}}) {
+    elements {
+      invitation_title {
+        text
+      }
+      user_account_input_label {
+        text
+      }
+      confirmation_button_text {
+        text
+      }
+      organization_logo {
+        ... on Node {
+          ... on KenticoCloudItemImageLink {
+            elements {
+              image {
+                assets {
+                  name
+                  url
+                }
+              }
+              image_url {
+                text
+              }
+            }
+          }
+        }
+      }
+      background {
+        assets {
+          name
+          url
+        }
+      }
+    }
+  }
+}
+`}
+    render={({
+      kenticoCloudItemInvitationPage: {
+        elements: {
+          invitation_title: {
+            text: invitationText
+          },
+          user_account_input_label: {
+            text: userInputLabelText
+          },
+          confirmation_button_text: {
+            text: confirmationButtonText
+          },
+          organization_logo: organization_logos,
+          background: {
+            assets: backgroundAssets
+          }
+        }
+      }
+    }) => {
+      const {
+        elements: {
+          image: {
+            assets: organizationLogos
+          },
+          image_url: {
+            text: organizationUrl
+          }
+        }
+      } = organization_logos[0];
+
+      const organization = organizationUrl.split('/').pop();
+
+      return (
+        <>
+          <Invitation
+            organization={organization}
+            invitationText={invitationText}
+            userInputLabelText={userInputLabelText}
+            confirmationButtonText={confirmationButtonText}
+            organizationLogoUrl={organizationLogos[0].url}
+            organizationUrl={organizationUrl}
+            backgroundUrl={backgroundAssets[0].url}
+          />
+        </>
+      );
+    }}
+  />
+);
+
+export default InvitationPage;
