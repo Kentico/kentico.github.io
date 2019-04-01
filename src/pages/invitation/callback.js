728f225d9640fe09fe49251a89e32033039cbe4f
Simply007 <chrastina.ondra@gmail.com>
2019-03-31 20:55:43 -0400

    tune up all states for invitation callback

--- src/pages/invitation/callback.js
+++ src/pages/invitation/callback.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { StaticQuery, graphql, Link } from 'gatsby'
 import qs from 'qs';
 import { invitationUrl } from "../../utils/config.json";
 import Axios from 'axios';
+import Loader from 'react-loaders';
+
+import './callback.scss';
 
 class InvitationCallback extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    let initialState = {
       loading: true,
       error: false,
       message: ''
@@ -27,28 +31,112 @@ class InvitationCallback extends Component {
           this.setState({
             error: true,
             loading: false,
-            message: `An error occured when sending invitation request ${JSON.stringify(err)}`
+            message: `An error occurred when sending invitation request ${JSON.stringify(err)}`
           })
         })
     }
+    else {
+      initialState = {
+        ...initialState,
+        error: true,
+        loading: false,
+        message: `This page is used for invitation callback.`
+      };
+    }
+
+    this.state = initialState;
   }
 
 
   render() {
 
+    const navigation = (
+      <ul>
+        <li>
+          <Link className="btn" to="/invitation">Go to Invitation page</Link>
+        </li>
+        <li>
+          <Link className="btn" to="/">Go to Home page</Link>
+        </li>
+      </ul>
+    );
+
+    let content = (
+      <div>
+        <h1>Success</h1>
+        <div>{this.state.message}</div>
+        {navigation}
+      </div>
+    );
+
+
     if (this.state.loading) {
-      return <h1 style={{ color: "#000000" }}>LOADING</h1>;
+      content = (<>
+        <Loader
+          type="ball-scale-ripple-multiple"
+          active={true}
+          className="loader"
+        />
+        <div>
+          <h1>Loading</h1>
+        </div>
+      </>
+      );
     }
-    
+
     if (this.state.error) {
-      return <h1 style={{ color: "#FF0000" }}>Error</h1>;
+      content = (
+        <div className="error">
+          <h1>Error</h1>
+          <div className="message">{this.state.message}</div>
+          {navigation}
+        </div>
+      );
     }
 
     return (
-      <div style={{ color: "#000000" }}>
-        <h1>{this.state.message}</h1>
-      </div>
-    )
+
+      <StaticQuery
+        query={graphql`
+{
+  kenticoCloudItemInvitationPage(system: {codename: {eq: "kentico_invitation_page"}}) {
+    elements{
+      background {
+        assets {
+          name
+          url
+        }
+      }
+    }
+  }
+}
+  `}
+        render={({
+          kenticoCloudItemInvitationPage: {
+            elements: {
+              background: {
+                assets: backgroundAssets
+              }
+            }
+          }
+        }) => {
+          return (
+            <div className="invitation-callback"
+              style={{
+                background: `url("${backgroundAssets[0].url}")
+                  center 
+                  bottom 
+                  no-repeat
+                  rgb(28, 38, 63)`
+              }}>
+              <div className="callback-content">
+                {content}
+              </div>
+            </div>
+          )
+        }}
+      />
+    );
   }
 }
 
